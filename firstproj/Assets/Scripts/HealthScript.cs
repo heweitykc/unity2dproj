4174bf378d5750115d162adb8a4845028b42b30d
heweitykc <heweitykc@163.com>
2013-12-30 11:04:38 +0800

    s

---
+++ firstproj/Assets/Scripts/HealthScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthScript : MonoBehaviour {
+	public int hp = 1;
+	public bool isEnemy = true;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+		ShotScript shot = collider.gameObject.GetComponent<ShotScript> ();
+		if (shot.isEnemyShot != isEnemy) {
+			hp -= shot.damage;
+			Destroy(shot.gameObject);
+			if(hp<=0){
+				Destroy(gameObject);
+			}
+		}
+	}
+}
